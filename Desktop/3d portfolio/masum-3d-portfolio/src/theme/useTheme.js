bb0fda4136f6c56f2f2b7d46715ce645fe394108
Tushar Spyveb <mobilionsteam@gmail.com>
2025-12-15 00:07:56 +0530

    Remove accidental Desktop folder from repository

--- Desktop/3d portfolio/masum-3d-portfolio/src/theme/useTheme.js
+++
@@ -1,6 +0,0 @@
-import { themes } from "./colors";
-
-export function useTheme() {
-  // change this ONE word to switch entire site color
-  return themes.purple; // cyan | purple | green | orange
-}
